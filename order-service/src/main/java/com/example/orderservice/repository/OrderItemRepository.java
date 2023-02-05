831ec04e80445c362f32192b7ad3fe838120a944
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2023-02-05 11:56:34 +0200

    emty master

--- order-service/src/main/java/com/example/orderservice/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.orderservice.repository;
-
-import com.example.orderservice.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
-}

79da44b797f8b7525cb37f856c1f9dd63bc7ef02
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2023-02-05 21:16:23 +0200

    add kafka

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -1,8 +1,11 @@
 package com.example.orderservice.controller;
 
+import com.example.orderservice.dto.OrderEvent;
 import com.example.orderservice.dto.OrderItemDto;
 import com.example.orderservice.dto.OrderRequest;
 import com.example.orderservice.dto.OrderResponse;
+import com.example.orderservice.kafka.OrderProducer;
+import com.example.orderservice.mapper.OrderMapper;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
@@ -17,38 +20,51 @@ import java.util.List;
 public class OrderController {
     private final OrderService orderService;
 
+    private final OrderProducer orderProducer;
+    private final OrderMapper orderMapper;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+
+
+        OrderEvent orderEvent=new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("order status is in pending");
+        orderEvent.setOrder(orderRequest);
+
+        orderProducer.sendMessage(orderEvent);
+
         orderService.placeOrder(orderRequest);
         return "Order place successfully";
     }
 
     @PostMapping("/{id}/add")
-    public String addOrderItem(@RequestBody OrderItemDto orderItemDto,@PathVariable long id){
-        orderService.addOrderItem(id,orderItemDto);
+    public String addOrderItem(@RequestBody OrderItemDto orderItemDto, @PathVariable long id) {
+        orderService.addOrderItem(id, orderItemDto);
         return "Order item was successfully added";
     }
+
     @DeleteMapping("{orderId}/remove/{orderItemId}")
-    public String deleteOrderItem(@PathVariable long orderId, @PathVariable long orderItemId){
-        orderService.deleteOrderItem(orderId,orderItemId);
-        return "item "+orderItemId+" deleted from order";
+    public String deleteOrderItem(@PathVariable long orderId, @PathVariable long orderItemId) {
+        orderService.deleteOrderItem(orderId, orderItemId);
+        return "item " + orderItemId + " deleted from order";
     }
 
     @GetMapping
-    public List<OrderResponse> getAllOrders(){
+    public List<OrderResponse> getAllOrders() {
         return orderService.getAllOrders();
     }
 
     @GetMapping("/{id}")
-    public OrderResponse getOrderById(@PathVariable long id){
+    public OrderResponse getOrderById(@PathVariable long id) {
         return orderService.getById(id);
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
-    public String removeOrder(@PathVariable long id){
+    public String removeOrder(@PathVariable long id) {
         orderService.deleteOrderById(id);
-        return "Order with id:"+id+" deleted";
+        return "Order with id:" + id + " deleted";
     }
 }
--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor

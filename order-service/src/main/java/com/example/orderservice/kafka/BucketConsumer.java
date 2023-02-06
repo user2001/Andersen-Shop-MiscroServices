8907b43d741d5c686d7beb7c2aad5be19f9c3456
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2023-02-06 16:43:39 +0200

    logic for add product was corrected

--- order-service/src/main/java/com/example/orderservice/kafka/BucketConsumer.java
+++ order-service/src/main/java/com/example/orderservice/kafka/BucketConsumer.java
@@ -2,13 +2,13 @@ package com.example.orderservice.kafka;
 
 import com.example.bucketservice.dto.BucketEvent;
 import com.example.bucketservice.dto.BucketStatus;
-import com.example.orderservice.dto.OrderRequest;
 import com.example.orderservice.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,11 +17,12 @@ import java.util.Map;
 public class BucketConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(BucketConsumer.class);
     private final OrderService orderService;
-    private Map<Long, List<Long>> orders = new HashMap<>();
+    private final Map<Long, List<Long>> orders = new HashMap<>();
 
     public BucketConsumer(OrderService orderService) {
         this.orderService = orderService;
     }
+
 
     @KafkaListener(topics = "${spring.kafka.topic.name}",
             groupId = "${spring.kafka.consumer.group-id}")
@@ -31,19 +32,29 @@ public class BucketConsumer {
 
         BucketStatus bucketStatus = event.getBucketStatus();
 
+        long userId = event.getUserId();
+        long productId = event.getProductId();
+        List<Long> idOfProducts = orders.get(userId);
+
         switch (bucketStatus) {
-            case ADD -> orders.put(event.getUserId(),
-                    List.of(event.getProductId()));
-            case REMOVE -> {
-                List<Long> productsId = orders.get(event.getUserId());
-                productsId.remove(event.getProductId());
-                orders.replace(event.getUserId(), productsId);
-            }
-            case SUBMIT -> {
-                OrderRequest orderRequest = new OrderRequest(
-                        event.getUserId(), orders.get(event.getUserId()));
-                orderService.placeOrder(orderRequest);
-            }
+            case ADD:
+                if (!orders.containsKey(userId)) {
+                    List<Long> productsId = new ArrayList<>();
+                    productsId.add(productId);
+                    orders.put(userId, productsId);
+                } else {
+                    idOfProducts.add(productId);
+                    orders.put(userId, idOfProducts);
+                }
+                break;
+            case REMOVE:
+                idOfProducts.remove(productId);
+                orders.put(userId, idOfProducts);
+                break;
+
+            case SUBMIT:
+                orderService.placeOrder(userId, idOfProducts);//save order to db
+                break;
         }
 
     }
